9f31a2bd23fd5bc77e69bd1e46fa505427cc9ae4
Your Name <you@example.com>
2018-08-18 16:01:35 -0400

    nightly build

--- packetstats.cpp
+++ packetstats.cpp
@@ -16,6 +16,8 @@
 #include <vector>
 
 using namespace std;
+
+
 #define TCP_FIN  (0x1 << 0)  
 #define TCP_SYN  (0x1 << 1)  
 #define TCP_RST  (0x1 << 2) 
@@ -25,31 +27,53 @@ using namespace std;
 #define TCP_ECE  (0x1 << 6) 
 #define TCP_CWR  (0x1 << 7) 
 
+struct udphdr {
+	uint16_t 	src;
+	uint16_t 	dst;
+	uint16_t 	len;
+	uint16_t	check;
+}__attribute__((packed));
+
+struct tcphdr {
+		uint16_t                src;
+		uint16_t                dst;
+		uint16_t                seq;
+		uint32_t                ack_seq;
+		uint8_t                 hl;                     //tcp header length
+		uint8_t                 Flags;
+		uint16_t                window;
+		uint16_t                check;
+		uint16_t                urg_ptr;
+} __attribute__((packed));
+
 struct	ether_header {
 	u_char	ether_dhost[6];
 	u_char	ether_shost[6];
 	u_short	ether_type;
-};
+}__attribute__((packed));
+
 struct	ether_header_vlan {
 	u_char	ether_dhost[6];
 	u_char	ether_shost[6];
 	uint16_t vlan_proto;
 	uint16_t vlan_tci;
 	uint16_t ether_type;
-};
+}__attribute__((packed));
+
 struct iphdr {
-        uint8_t                 ip_hl:4; /* both fields are 4 bits */
-        uint8_t                 ip_v:4;
-        uint8_t                 ip_tos;
-        uint16_t                ip_len;
-        uint16_t                ip_id;
-        uint16_t                ip_off;
-        uint8_t                 ip_ttl;
-        uint8_t                 ip_p;
-        uint16_t                ip_sum;
-        struct in_addr  ip_src;
-        struct in_addr  ip_dst;
-};
+		uint8_t                 ip_hl:4; /* both fields are 4 bits */
+		uint8_t                 ip_v:4;
+		uint8_t                 ip_tos;
+		uint16_t                ip_len;
+		uint16_t                ip_id;
+		uint16_t                ip_off;
+		uint8_t                 ip_ttl;
+		uint8_t                 ip_p;
+		uint16_t                ip_sum;
+		struct in_addr  ip_src;
+		struct in_addr  ip_dst;
+} __attribute__((packed));
+
 #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
 #define BYTE_TO_BINARY(byte)  \
   (byte & 0x80 ? '1' : '0'), \
@@ -60,20 +84,12 @@ struct iphdr {
   (byte & 0x04 ? '1' : '0'), \
   (byte & 0x02 ? '1' : '0'), \
   (byte & 0x01 ? '1' : '0') 
-struct tcphdr {
-        uint16_t                source;
-        uint16_t                dest;
-        uint16_t                seq;
-        uint32_t                ack_seq;
-        uint8_t                 hl;                     //tcp header length
-        uint8_t                 Flags;
-        uint16_t                window;
-        uint16_t                check;
-        uint16_t                urg_ptr;
-} ;
+
 
 struct iphdr *ip;
 struct tcphdr *tcp;
+struct udphdr *udp;
+
 char errbuf[PCAP_ERRBUF_SIZE];
 pcap_t *pcap;
 struct pcap_pkthdr hdr;
@@ -87,8 +103,11 @@ struct ether_header_vlan *ether_header_vlan;
 std::map <std::string,int> Out;
 std::map <std::string,int> DstIps;
 std::map <std::string,int> SrcIps;
+std::map <int,int> TcpSrcPorts;
+std::map <int,int> UdpSrcPorts;
 std::vector<std::pair<std::string, int> > TopDstIpsPackets(20);
 std::vector<std::pair<std::string, int> > TopSrcIpsPackets(20);
+std::vector<std::pair<int , int> > TopUdpSrcPorts(20);
 
 void PrintJson(){
 
@@ -107,92 +126,65 @@ void PrintJson(){
 	std::cout << "\t\"TopDstIpsPackets\": { " <<std::endl;
 	
 	for (std::vector<std::pair<std::string, int> >::iterator it = TopDstIpsPackets.begin() ; it != TopDstIpsPackets.end(); ++it)
-    {
-        std::cout <<"\t\t\""<< it->first << "\":" << it->second;
-        if(++it<TopDstIpsPackets.end()) {
-        	if(it->second!=0) cout<<","; else {
-        		cout<<endl;
-        		break;
-        	}
-        }
-        it--;
-        cout<<endl;
-    }
+	{
+		std::cout <<"\t\t\""<< it->first << "\":" << it->second;
+		if(++it<TopDstIpsPackets.end()) {
+			if(it->second!=0) cout<<","; else {
+				cout<<endl;
+				break;
+			}
+		}
+		it--;
+		cout<<endl;
+	}
 	cout<<"\t},"<<endl;
 
 
 	//top source ips (packets)
 	std::cout << "\t\"TopSrcIpsPackets\": { " <<std::endl;
 	for (std::vector<std::pair<std::string, int> >::iterator it = TopSrcIpsPackets.begin() ; it != TopSrcIpsPackets.end(); ++it)
-    {
-        std::cout <<"\t\t\""<< it->first << "\":" << it->second;
-        if(++it<TopSrcIpsPackets.end()) cout<<",";
-        it--;
-        cout<<endl;
-    }
+	{
+		std::cout <<"\t\t\""<< it->first << "\":" << it->second;
+		if(++it<TopSrcIpsPackets.end()) cout<<",";
+		it--;
+		cout<<endl;
+	}
+	cout<<"\t}"<<endl;
+
+	//top src udp ports
+	std::cout << "\t\"TopUdpSrcPorts\": { " <<std::endl;
+	for (std::vector<std::pair<int, int> >::iterator it = TopUdpSrcPorts.begin() ; it != TopUdpSrcPorts.end(); ++it)
+	{
+		std::cout <<"\t\t\""<< it->first << "\":" << it->second;
+		if(++it<TopUdpSrcPorts.end()) {
+			if(it->first!=0) cout<<","; else {
+				cout<<endl;
+				break;
+			}
+		}
+		it--;
+		cout<<endl;
+	}
 	cout<<"\t}"<<endl;
 
 
+
+	//end !?
+
+
 	cout<<"}"<<endl;
-	
+	//cout<<"Src ips"<<SrcIps.size();
+	/*
+	for(std::map<int,int>::iterator it=SrcPorts.begin();it!=SrcPorts.end();it++){
+		cout<<it->first<< " " << it->second<<std::endl;
+		
+	}*/
 }
 
 int main(int argc, char **argv){
 
 //for s in `cat packetstats.cpp |grep 'Out\[\"[0-9a-zA-Z_-]\{0,120\}\"\]' -o |sort -u `;do  echo $s=0\; ;done
 
-Out["L2_ARP-Bytes"]=0;
-Out["L2_ARP-Packets"]=0;
-Out["L2_LLDP-Bytes"]=0;
-Out["L2_LLDP-Packets"]=0;
-Out["L2_OTHER-Bytes"]=0;
-Out["L2_OTHER-Packets"]=0;
-Out["L3_IP-Bytes"]=0;
-Out["L3_IP-Packets"]=0;
-Out["L3_IPv4-Bytes"]=0;
-Out["L3_IPv4-Packets"]=0;
-Out["L3_IPv6-Bytes"]=0;
-Out["L3_IPv6-Packets"]=0;
-Out["L4_ICMP-Bytes"]=0;
-Out["L4_ICMP-Packets"]=0;
-Out["L4_IGMP-Bytes"]=0;
-Out["L4_IGMP-Packets"]=0;
-Out["L4_OTHER-Bytes"]=0;
-Out["L4_OTHER-Packets"]=0;
-Out["L4_TCP-Bytes"]=0;
-Out["L4_TCP-Packets"]=0;
-Out["L4_UDP-Packets"]=0;
-Out["L4_UDP-UdpBytes"]=0;
-Out["TCP_FLAGS-ACK-Bytes"]=0;
-Out["TCP_FLAGS-ACK-ECE-Bytes"]=0;
-Out["TCP_FLAGS-ACK-ECE-Packets"]=0;
-Out["TCP_FLAGS-ACK-Packets"]=0;
-Out["TCP_FLAGS-ACK-RST-Bytes"]=0;
-Out["TCP_FLAGS-ACK-RST-Packets"]=0;
-Out["TCP_FLAGS-FIN-ACK-Bytes"]=0;
-Out["TCP_FLAGS-FIN-ACK-Packets"]=0;
-Out["TCP_FLAGS-FIN-Bytes"]=0;
-Out["TCP_FLAGS-FIN-Packets"]=0;
-Out["TCP_FLAGS-FIN-PSH-ACK-Bytes"]=0;
-Out["TCP_FLAGS-FIN-PSH-ACK-Packets"]=0;
-Out["TCP_FLAGS-FIN-PSH-Bytes"]=0;
-Out["TCP_FLAGS-FIN-PSH-Packets"]=0;
-Out["TCP_FLAGS-OTHER-Bytes"]=0;
-Out["TCP_FLAGS-OTHER-Packets"]=0;
-Out["TCP_FLAGS-PSH-ACK-Bytes"]=0;
-Out["TCP_FLAGS-PSH-ACK-Packets"]=0;
-Out["TCP_FLAGS-PSH-URG-Bytes"]=0;
-Out["TCP_FLAGS-PSH-URG-Packets"]=0;
-Out["TCP_FLAGS-RST-Bytes"]=0;
-Out["TCP_FLAGS-RST-Packets"]=0;
-Out["TCP_FLAGS-SYN-ACK-Bytes"]=0;
-Out["TCP_FLAGS-SYN-ACK-Packets"]=0;
-Out["TCP_FLAGS-SYN-Bytes"]=0;
-Out["TCP_FLAGS-SYN-Packets"]=0;
-Out["TooSmallFrames"]=0;
-Out["TOTAL-Bytes"]=0;
-Out["TOTAL-Packets"]=0;
-Out["VlanTagged"]=0;
 
 
 	pcap=pcap_open_offline(argv[1], errbuf);
@@ -245,6 +237,7 @@ Out["VlanTagged"]=0;
 						Out["L3_IPv4-Bytes"]+=(hdr.caplen-eth_len);
 						DstIps[inet_ntoa(ip->ip_dst)]++;
 						SrcIps[inet_ntoa(ip->ip_src)]++;
+					
 						switch(ip->ip_p){
 							case 0x01:
 								Out["L4_ICMP-Packets"]++;
@@ -255,9 +248,12 @@ Out["VlanTagged"]=0;
 								Out["L4_IGMP-Bytes"]+=(hdr.caplen-eth_len);
 								break;
 							case 0x06:
+								/* this "correct" only because size of ip header  = size of tcp header */
+								tcp=(struct tcphdr*)ip+1;
+								TcpSrcPorts[htons(tcp->src)]++;
 								Out["L4_TCP-Packets"]++;
 								Out["L4_TCP-Bytes"]+=(hdr.caplen-eth_len);
-								tcp=(struct tcphdr*)ip+1;
+								
 								switch(tcp->Flags){
 									case TCP_SYN:
 										Out["TCP_FLAGS-SYN-Packets"]++;
@@ -317,11 +313,26 @@ Out["VlanTagged"]=0;
 							case 0x11:
 								Out["L4_UDP-Packets"]++;
 								Out["L4_UDP-UdpBytes"]+=(hdr.caplen-eth_len);
+								udp=(struct udphdr *)((char *)ip+sizeof(struct iphdr));
+								switch(ntohs(ip->ip_off)){
+									case 0x4000://df 
+									case 0x2000://mf
+									case 0x0000://no fragments
+										UdpSrcPorts[ntohs(udp->src)]++;
+										//printf("%d\n",ntohs(udp->src));
+										break;
+									default:
+											Out["UDP_Fragmented-Packets"]++;
+										break;
+
+
+								}
+
+
 								break;
 							default:
 								Out["L4_OTHER-Packets"]++;
 								Out["L4_OTHER-Bytes"]++;
-								//printf("%.2x\n",ip->ip_p);
 								break;
 						}
 						break;
@@ -361,24 +372,37 @@ Out["VlanTagged"]=0;
 	don't judge me monkey
 	*/
 	std::partial_sort_copy(DstIps.begin(),
-                           DstIps.end(),
-                           TopDstIpsPackets.begin(),
-                           TopDstIpsPackets.end(),
-                           [](std::pair<const std::string, int> const& l,
-                              std::pair<const std::string, int> const& r)
-                           {
-                               return l.second > r.second;
-                           });
+						   DstIps.end(),
+						   TopDstIpsPackets.begin(),
+						   TopDstIpsPackets.end(),
+						   [](std::pair<const std::string, int> const& l,
+							  std::pair<const std::string, int> const& r)
+						   {
+							   return l.second > r.second;
+						   });
 	/* top src ips list */
 	std::partial_sort_copy(SrcIps.begin(),
-                           SrcIps.end(),
-                           TopSrcIpsPackets.begin(),
-                           TopSrcIpsPackets.end(),
-                           [](std::pair<const std::string, int> const& l,
-                              std::pair<const std::string, int> const& r)
-                           {
-                               return l.second > r.second;
-                           });
+						   SrcIps.end(),
+						   TopSrcIpsPackets.begin(),
+						   TopSrcIpsPackets.end(),
+						   [](std::pair<const std::string, int> const& l,
+							  std::pair<const std::string, int> const& r)
+						   {
+							   return l.second > r.second;
+						   });
+
+	/* top src ports */
+		std::partial_sort_copy(UdpSrcPorts.begin(),
+						   UdpSrcPorts.end(),
+						   TopUdpSrcPorts.begin(),
+						   TopUdpSrcPorts.end(),
+						   [](std::pair<const int , int> const& l,
+							  std::pair<const int, int> const& r)
+						   {
+							   return l.second > r.second;
+						   });
+
+
 	  PrintJson();
 
 
